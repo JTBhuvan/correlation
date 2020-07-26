9c5969412179fbdc484fa6580b7b76b8d541f042
JTBhuvan <59383522+JTBhuvan@users.noreply.github.com>
2020-07-26 19:56:23 +0530

    Add files via upload

---
+++ sizeOfTvVsTimeSpent.py
@@ -0,0 +1,25 @@
+import csv
+import numpy as np
+
+def getDataSource(data_path):
+    size_of_tv = []
+    average_time_spent = []
+    with open(data_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            size_of_tv.append(float(row["Size of TV"]))
+            average_time_spent.append(float(row["\tAverage time spent watching TV in a week (hours)"]))
+
+    return {"x" : size_of_tv, "y": average_time_spent}
+
+def findCorrelation(datasource):
+    correlation = np.corrcoef(datasource["x"], datasource["y"])
+    print("Correlation between Sizeof tv vs Average time spent :-  \n--->",correlation[0,1])
+
+def setup():
+    data_path  = "./data/Size of TV,_Average time spent watching TV in a week (hours).csv"
+
+    datasource = getDataSource(data_path)
+    findCorrelation(datasource)
+
+setup()
